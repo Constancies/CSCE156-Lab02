c78bf40fd7edfca9564ff3245081ee1a55240687
Alexander Sanderson <alexander.sanderson@outlook.com>
2025-01-29 15:23:16 -0600

    add solutions

--- python/child_credit.py
+++ python/child_credit.py
@@ -3,7 +3,28 @@
 from child import Child
 
 def produceReport(children):
-    #TODO
+    credit_values = list()
+    bonus_given = False
+
+    for i in children:
+        if i.age < 18 and bonus_given == False:
+            credit_values.append(1000.00)
+            bonus_given = True
+        elif i.age < 18:
+            credit_values.append(500.00)
+        else:
+            credit_values.append(0.00)
+
+    total_credit = 0
+    for i in range(len(credit_values)):
+        total_credit += credit_values[i]
+
+    report = "Child          Amount   "
+    for i in range(len(children)):
+        report += f"\n{children[i]}   $ {credit_values[i]}"
+    report += f"\nTotal Credit:    $ {total_credit}"
+    return total_credit
+    
 
 def main():
 
@@ -14,6 +35,7 @@ def main():
     children = [tom, dick, harry]
 
     # TODO: call your function here
+    print(produceReport(children))
 
 if __name__ == "__main__":
     main()
--- python/natural.py
+++ python/natural.py
@@ -1,14 +1,23 @@
 import sys
 
 def sumWithFor(n):
-    # TODO: implement
+    sum = 0
+    for i in range(n + 1):
+        sum += i
+    return sum
 
 def sumWithWhile(n):
-    # TODO: implement
+    sum = 0
+    i = 0
+    while i < n + 1:
+        sum += i
+        i += 1
+    return sum
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        sys.stderr.write("Provide n as a command line argument.")
+        sys.stderr.write("Provide n as a command line argument.\n")
         exit()
     n = int(sys.argv[1])
-    # TODO: call your functions and output the results
+    print(sumWithFor(n))
+    print(sumWithWhile(n))
